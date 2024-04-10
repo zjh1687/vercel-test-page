509e810571067cee02f853c5f6368715423e6106
seungcheol <zjh1687@naver.com>
2024-04-10 18:38:46 +0900

    업데이트

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals(sendToVercelAnalytics);
